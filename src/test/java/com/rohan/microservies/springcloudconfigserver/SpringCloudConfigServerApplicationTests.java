bc2f05fcef5c5ee18d9b5988c6a972d4ec04fb2d
Rohan <arohan.mca@gmail.com>
2021-02-21 12:53:43 +0530

    spring-cloud-config-server

---
+++ src/test/java/com/rohan/microservies/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohan.microservies.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
